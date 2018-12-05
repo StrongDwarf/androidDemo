b1aa1b97a3891dfe240496a6854f5097b2bb6134
xiaobaicai <1908654110@qq.com>
2018-12-05 19:05:27 +0800

    init

---
+++ app/src/main/java/com/demo/xiaobaicai/androiddemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.demo.xiaobaicai.androiddemo;
+
+import android.app.ActivityOptions;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.ContactsContract;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.LiveFolders.INTENT;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.test2:
+
+                break;
+        }
+    }
+
+}
